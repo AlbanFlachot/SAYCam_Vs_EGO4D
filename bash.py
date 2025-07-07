d791fafe9c255070e4cdd15f6676280c639a3364
AlbanFlachot <flachot.alban@gmail.com>
2025-07-07 23:48:07 +0200

    RDMs

--- bash.py
+++ bash.py
@@ -2,8 +2,8 @@ import subprocess
 import shlex
 import os
 
-model = 'saycam'
-dataset = 'hmIT_Translationtest'
+model = 'ego'
+dataset = 'hmIT_stimuli'
 command = f"python3 save_activations.py --model {model} --dataset {dataset}"
 args = shlex.split(command)
 subprocess.call(args)
