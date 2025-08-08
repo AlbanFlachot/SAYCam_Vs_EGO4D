3875cd774fb27978eda88f5d5574db55b533c6f7
AlbanFlachot <flachot.alban@gmail.com>
2025-08-08 15:51:18 +0200

    datadriven approach

--- bash.py
+++ bash.py
@@ -2,14 +2,25 @@ import subprocess
 import shlex
 import os
 
-
+'''
+### Save activations for RSA on specific datasets
 for model in ['ego', 'saycam', 'random', 'imagenet', 's', 'a', 'y']:
 #for model in ['supervised']:
     for dataset in ['MEG_face_stim', '100_faces', 'hmIT_stimuli', '100_objects']:
     #for dataset in ['MEG_face_stim']:
-        gpu = 3
+        gpu = 7
         # command = f"python3 save_activations.py --model {model} --dataset {dataset}" # if don't specify GPU
         command = f"python3 save_activations.py --model {model} --dataset {dataset} --cuda --gpuid {gpu}" # if want to use a GPU
         args = shlex.split(command)
-        subprocess.call(args)
+        subprocess.call(args)'''
 
+### Save activations for datadriven approach
+for model in ['ego', 'saycam', 'imagenet', 'supervised', 'random']:
+#for model in ['supervised']:
+    for dataset in ['ecoVal']:
+    #for dataset in ['MEG_face_stim']:
+        gpu = 7
+        # command = f"python3 save_activations.py --model {model} --dataset {dataset}" # if don't specify GPU
+        command = f"python3 save_activations_datadriven.py --model {model} --dataset {dataset} --cuda --gpuid {gpu}" # if want to use a GPU
+        args = shlex.split(command)
+        subprocess.call(args)
