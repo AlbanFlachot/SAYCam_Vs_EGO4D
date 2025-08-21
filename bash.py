17218a4d5f11e6dfd873774f1ea5a4c384649ae5
AlbanFlachot <flachot.alban@gmail.com>
2025-08-21 18:32:26 +0200

    added requirements.txt

--- bash.py
+++ bash.py
@@ -15,11 +15,11 @@ for model in ['ego', 'saycam', 'random', 'imagenet', 's', 'a', 'y']:
         subprocess.call(args)'''
 
 ### Save activations for datadriven approach
-for model in ['ego', 'saycam', 'imagenet', 'supervised', 'random']:
+for model in ['resnet']:#['ego', 'saycam', 'imagenet', 'supervised', 'random']:
 #for model in ['supervised']:
     for dataset in ['ecoVal']:
     #for dataset in ['MEG_face_stim']:
-        gpu = 7
+        gpu = 0
         # command = f"python3 save_activations.py --model {model} --dataset {dataset}" # if don't specify GPU
         command = f"python3 save_activations_datadriven.py --model {model} --dataset {dataset} --cuda --gpuid {gpu}" # if want to use a GPU
         args = shlex.split(command)
